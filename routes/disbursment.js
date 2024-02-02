b586afaedcedbedd0afa663142d0690ad52bd81f
jephthah-snr <60290652+jephthah-snr@users.noreply.github.com>
2024-02-02 23:10:25 +0100

    removed validator import and use in preHandler

--- routes/disbursment.js
+++ routes/disbursment.js
@@ -1,12 +1,12 @@
 const Base = require("../controllers.js/basecontroller");
-const validator = require("../../validator.js/customValidator"
+
 
 
 async function routes(fastify, options) {
   fastify.route({
     url: '/',
     method: 'GET',
-    preHandler: [validator()],
+    preHandler: [],
     handler: Base.main,
   });
 }
